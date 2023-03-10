03ede9994e9f789db511e660c8487aea4dfe2ba3
Vitaly Maksimov <2california@mail.ru>
2023-03-11 03:12:24 +0600

    base structure ecommerce

---
+++ components/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+	 <div>Productgg</div>
+  )
+}
+
+export default Product;
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hm42f1li',
+    dataset: 'production'
+  }
+})
