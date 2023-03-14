e3a2068cbddef175512b3905fd25604ada165ed5
Vitaly Maksimov <2california@mail.ru>
2023-03-14 17:36:50 +0600

    slug, statecontext, productCard

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import { Cart } from "./";
+// import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+//   const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
-	 <div>Navbar</div>
-  )
-}
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Headphones</Link>
+      </p>
+
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        {/* <span className="cart-item-qty">{totalQuantities}1</span> */}
+        <span className="cart-item-qty">1</span>
+      </button>
+
+      {/* {showCart && <Cart />} */}
+    </div>
+  );
+};
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import '@/styles/globals.css'
+import { Toaster } from "react-hot-toast";
+
+import { Layout } from '@/components';
+
+import { StateContext } from '@/context/StateContext';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  	return (
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+   );
 }
